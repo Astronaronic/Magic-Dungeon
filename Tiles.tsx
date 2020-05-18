fd2a704ad3c31b913ec011f3a2d619e4f4e30117
Astronaronic <51531461+Astronaronic@users.noreply.github.com>
2020-05-18 14:16:40 +0100

    Add files via upload

---
+++ Tiles.tsx
@@ -0,0 +1,202 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Tiles" tilewidth="16" tileheight="16" tilecount="66" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="Sprites/wall_corner_front_right.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="Sprites/wall_corner_left.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="Sprites/wall_corner_right.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="Sprites/wall_corner_top_left.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="Sprites/wall_corner_top_right.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="Sprites/wall_coulmn_base.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="Sprites/wall_fountain_basin_blue_anim_f0.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="Sprites/wall_fountain_basin_blue_anim_f1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="Sprites/wall_fountain_basin_blue_anim_f2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="Sprites/wall_fountain_basin_red_anim_f0.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="Sprites/wall_fountain_basin_red_anim_f1.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="Sprites/wall_fountain_basin_red_anim_f2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="Sprites/wall_fountain_mid_blue_anim_f0.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="Sprites/wall_fountain_mid_blue_anim_f1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="Sprites/wall_fountain_mid_blue_anim_f2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="Sprites/wall_fountain_mid_red_anim_f0.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="Sprites/wall_fountain_mid_red_anim_f1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="Sprites/wall_fountain_mid_red_anim_f2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="Sprites/wall_fountain_top.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="Sprites/wall_goo.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="Sprites/wall_goo_base.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="Sprites/wall_hole_1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="Sprites/wall_hole_2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="Sprites/wall_inner_corner_l_top_left.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="Sprites/wall_inner_corner_l_top_rigth.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="Sprites/wall_inner_corner_mid_left.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="Sprites/wall_inner_corner_mid_rigth.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="Sprites/wall_inner_corner_t_top_left.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="16" source="Sprites/wall_inner_corner_t_top_rigth.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="Sprites/wall_left.png"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="Sprites/wall_mid.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="Sprites/wall_right.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="Sprites/wall_side_front_left.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="Sprites/wall_side_front_right.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="Sprites/wall_side_mid_left.png"/>
+ </tile>
+ <tile id="35">
+  <image width="16" height="16" source="Sprites/wall_side_mid_right.png"/>
+ </tile>
+ <tile id="36">
+  <image width="16" height="16" source="Sprites/wall_side_top_left.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="16" source="Sprites/wall_side_top_right.png"/>
+ </tile>
+ <tile id="38">
+  <image width="16" height="16" source="Sprites/wall_top_left.png"/>
+ </tile>
+ <tile id="39">
+  <image width="16" height="16" source="Sprites/wall_top_mid.png"/>
+ </tile>
+ <tile id="40">
+  <image width="16" height="16" source="Sprites/wall_top_right.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="Sprites/wall_banner_blue.png"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="Sprites/wall_banner_green.png"/>
+ </tile>
+ <tile id="43">
+  <image width="16" height="16" source="Sprites/wall_banner_red.png"/>
+ </tile>
+ <tile id="44">
+  <image width="16" height="16" source="Sprites/wall_banner_yellow.png"/>
+ </tile>
+ <tile id="45">
+  <image width="16" height="16" source="Sprites/wall_column_mid.png"/>
+ </tile>
+ <tile id="46">
+  <image width="16" height="16" source="Sprites/wall_column_top.png"/>
+ </tile>
+ <tile id="47">
+  <image width="16" height="16" source="Sprites/wall_corner_bottom_left.png"/>
+ </tile>
+ <tile id="48">
+  <image width="16" height="16" source="Sprites/wall_corner_bottom_right.png"/>
+ </tile>
+ <tile id="49">
+  <image width="16" height="16" source="Sprites/wall_corner_front_left.png"/>
+ </tile>
+ <tile id="50">
+  <image width="16" height="16" source="Sprites/floor_spikes_anim_f2.png"/>
+ </tile>
+ <tile id="51">
+  <image width="16" height="16" source="Sprites/floor_spikes_anim_f3.png"/>
+ </tile>
+ <tile id="52">
+  <image width="16" height="16" source="Sprites/floor_spikes_anim_f1.png"/>
+ </tile>
+ <tile id="53">
+  <image width="16" height="16" source="Sprites/floor_spikes_anim_f0.png"/>
+ </tile>
+ <tile id="54">
+  <image width="16" height="16" source="Sprites/floor_8.png"/>
+ </tile>
+ <tile id="55">
+  <image width="16" height="16" source="Sprites/floor_ladder.png"/>
+ </tile>
+ <tile id="56">
+  <image width="16" height="16" source="Sprites/floor_6.png"/>
+ </tile>
+ <tile id="57">
+  <image width="16" height="16" source="Sprites/floor_7.png"/>
+ </tile>
+ <tile id="58">
+  <image width="16" height="16" source="Sprites/floor_5.png"/>
+ </tile>
+ <tile id="59">
+  <image width="16" height="16" source="Sprites/floor_4.png"/>
+ </tile>
+ <tile id="60">
+  <image width="16" height="16" source="Sprites/floor_1.png"/>
+ </tile>
+ <tile id="61">
+  <image width="16" height="16" source="Sprites/floor_3.png"/>
+ </tile>
+ <tile id="62">
+  <image width="16" height="16" source="Sprites/floor_2.png"/>
+ </tile>
+ <tile id="63">
+  <image width="16" height="16" source="Sprites/wall_top_mid_up.png"/>
+ </tile>
+ <tile id="64">
+  <image width="16" height="16" source="Sprites/wall_side_lower_left.png"/>
+ </tile>
+ <tile id="65">
+  <image width="16" height="16" source="Sprites/wall_side_lower_right.png"/>
+ </tile>
+</tileset>
